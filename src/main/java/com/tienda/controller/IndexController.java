25a5edd7fa1519400181b0019977342422ab8571
justh <justh@LAPTOP-LNLIR2AK>
2023-10-11 19:46:04 -0600

    Quinto commit

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author justh
  */
 @Controller
-@RequestMapping("/Index")
 public class IndexController {
     
     @RequestMapping("/")
